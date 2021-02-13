2c9313eb1bb53a9cb9d509aa18f0d032e99ab17b
Brenda Goodwin <brendanced@gmail.com>
2021-02-12 23:37:25 -0700

    navbar started

---
+++ typing-test/src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from "reactstrap";
+
+const Navigation = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">TypeCheck</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink
+                href="https://github.com/IsraaBeseiso/typing-test-.git"
+                target="_blank"
+              >
+                GitHub Repository
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>My Dashboard</DropdownItem>
+                <DropdownItem>Goodbye</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Welcome!</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;

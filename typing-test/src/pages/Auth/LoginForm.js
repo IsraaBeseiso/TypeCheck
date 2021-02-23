d2cec5fcf3bae976f05e1512a93bf8292f5f79f0
Brenda Goodwin <brendanced@gmail.com>
2021-02-23 12:15:21 -0700

    updated package-lock with new dependancies

---
+++ typing-test/src/pages/Auth/LoginForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./LoginForm.css";
+import $ from "jquery";
+
+function LoginForm(props) {
+  $(".message a").click(function () {
+    $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
+  });
+
+  // Once API routes are defined they will be used for login
+
+  return (
+    <div class="login-page">
+      <div class="form">
+        <form class="register-form">
+          <input type="text" placeholder="name" />
+          <input type="password" placeholder="password" />
+          <input type="text" placeholder="email address" />
+          <button>create</button>
+          <p class="message">
+            Already registered? <a href="#">Sign In</a>
+          </p>
+        </form>
+        <form class="login-form">
+          <input type="text" placeholder="username" />
+          <input type="password" placeholder="password" />
+          <button>login</button>
+          <p class="message">
+            Not registered? <a href="#">Create an account</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginForm;

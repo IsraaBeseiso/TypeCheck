efc9eef52afd72bb7c2f1e4734365cb51ab607a8
Brenda Goodwin <brendanced@gmail.com>
2021-02-23 12:46:33 -0700

    working on app.js and component tree

--- typing-test/src/App.js
+++ typing-test/src/App.js
@@ -1,1 +1,23 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
+import LoginForm from "./pages/LoginForm";
+import Wrapper from "./components/Wrapper";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/loginForm" component={LoginForm} />
+        </Wrapper>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
--- typing-test/src/index.js
+++ typing-test/src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import LogInContainer from "./components/LogInContainer";
-import Navbar from "./components/Navbar";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <Navbar />
-      <LogInContainer />
-    </div>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );

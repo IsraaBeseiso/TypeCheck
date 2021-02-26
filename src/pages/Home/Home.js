9309a1c9ae7f40c844409cc41f5955503243c622
Israa Beseiso <israabeseiso@gmail.com>
2021-02-25 19:07:52 -0700

    added home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./style.css";
+
+const testStrings = [
+  'Type this test as fast as you can.',
+  'Type this one even faster if you can.'
+]
+
+const Home = () => {
+  const [testStringIndex, setTestStringIndex] = React.useState(0);
+
+  const handleCompleteTest = React.useCallback(() => {
+
+  }, []);
+
+  const handleCheckValidInput = React.useCallback((e) => {
+    const { value } = e.target;
+    if (value === testStrings[testStringIndex]) {
+      // Stop time
+      // POST the time to endpoint
+      // Increment testStringIndex
+
+      setTestStringIndex(testStringIndex + 1);
+    }
+  }, []);
+
+  return (
+    <div className="home-container">
+      <div className="text-container">
+        <p>{testStrings[testStringIndex]}</p>
+      </div>
+      <textarea className="text-area" onChange={handleCheckValidInput} />
+    </div>
+  )
+}
+
+export default Home;

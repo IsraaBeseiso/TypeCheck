368da70afaa3fc4d2c6f42934610eaedf368c9c2
Brenda Goodwin <brendanced@gmail.com>
2021-02-26 13:19:52 -0700

    still fix git

---
+++ src/components/Scoretables/ScoreTable.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Row, Col, Container } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./ScoreTable.css";
+import OverallTable from "./OverallTable";
+import PersonalTable from "./PersonalTable";
+
+function Scoretable() {
+  // dummy hish scores
+  return (
+    <div>
+      <Container
+        className="scorecol ml-0 
+      col-7 text-center"
+      >
+        <Row className="justify-content-xs-left">
+          <Col sm="2">
+            <h1>Normal Mode!</h1>
+            <h5>High Scores</h5>
+            <h3>Overall</h3>
+            <OverallTable />
+
+            <h3>Personal</h3>
+            <PersonalTable />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Scoretable;

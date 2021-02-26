74056360ab4db7342e4d3758c3b3f4f0fe3b0bc9
Tim Holzer <timiholzer@gmail.com>
2021-02-25 18:59:08 -0700

    updated routes and moved server.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const Highscore = require("../models/highscores.js");
+const Highscore = require("../models/highscore.js");
 
 //can't promise that this is correct yet
 
-router.post("/api/highscore", ({body}, res) => {
+router.post("/api/highscores", ({body}, res) => {
     Highscore.create(body)
       .then(dbHighscore => {
         res.json(dbHighscore);
@@ -13,7 +13,7 @@ router.post("/api/highscore", ({body}, res) => {
       });
   });
   
-  router.post("/api/highscore/bulk", ({body}, res) => {
+  router.post("/api/highscores/bulk", ({body}, res) => {
     Highscore.insertMany(body)
       .then(dbHighscore => {
         res.json(dbHighscore);
@@ -23,19 +23,73 @@ router.post("/api/highscore", ({body}, res) => {
       });
   });
   
-  router.get("/api/highscore", (req, res) => {
-    Highscore.find({}).sort({score: -1})
-      .then(dbHighscore => {
-        res.json(dbHighscore);
+
+  //get top 10
+  router.get("/api/highscores", (req, res) => {
+    Highscore.find().then(highscore => {
+
+    //returning the top 10 normal
+    const topnorm = highscore.map(i => {
+      const {username, score, type} = i
+      return {username, score, type}
+    })
+
+    //need a line to get rid of ones where type = false
+
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 10)
+    
+
+    //returning the top 10 chaos
+    const topchaos = highscore.map(i => {
+      const {username, score, type} = i
+      return {username, score, type}
+    })
+
+    //need a line to get rid of ones where type = true
+
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 10)
+    
+
+
+    //returning the top 10 personal norm
+    const toppersonalnorm = highscore.map(i => {
+      const {username, score, type} = i
+      return {username, score, type}
+    })
+
+    //need a line where it filters to only username = the logged in username and type = false
+
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 10)
+
+
+    //returning the top 10 personal norm
+    const toppersonalchaos = highscore.map(i => {
+      const {username, score, type} = i
+      return {username, score, type}
+    })
+
+    //need a line where it filters to only username = the logged in username and type = true
+
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 10)
+
+
+
+        return res.json(topnorm, topchaos, toppersonalnorm, toppersonalchaos);
+
       })
-      .catch(err => {
-        res.status(400).json(err);
-      });
+    // }
+    //   .catch(err => {
+    //     res.status(400).json(err);
+
   });
   
 //personal highscore route
 
-router.get("/api/personalhighscore/:param", (req, res) => {
+router.get("/api/personalhighscores/:param", (req, res) => {
   Highscore.find({username:param}).sort({date: -1})
     .then(dbPersonalHighscore => {
       res.json(dbPersonalHighscore);

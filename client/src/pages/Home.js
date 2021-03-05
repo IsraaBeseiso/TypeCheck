fe030fb363bb76c6360233ae6d3c576e1b86c8dd
Brenda Goodwin <brendanced@gmail.com>
2021-03-05 16:12:06 -0700

    Sass trials

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ import NormScoreTable from "../components/scoretables/NormScoreTable";
 import Timer from "../components/Timer";
 import { Row, Col } from "reactstrap";
 import TypingBox from "../components/typing/TypingBox";
-import "./Home.css";
+import "./Home.scss";
 
 function Home() {
   return (

f824fbaf79062ecab33458f2f0d3e5bac9e354df
Tim Holzer <timiholzer@gmail.com>
2021-03-08 12:44:50 -0700

    worked on bcrypt login

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -3,10 +3,43 @@ import "./Authentication.scss";
 import $ from "jquery";
 
 
+
+
+
+
 function LoginForm(props) {
   $(".message a").click(function () {
     $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
   });
+
+//   async function loginFormHandler(event) {
+//     event.preventDefault();
+  
+//     const username = document.querySelector('#username-login').value.trim();
+//     const email = document.querySelector('#email-login').value.trim();
+//     const password = document.querySelector('#password-login').value.trim();
+  
+//     if (username && email && password) {
+//       const response = await fetch('/api/login', {
+//         method: 'post',
+//         body: JSON.stringify({
+//           username,
+//           email,
+//           password
+//         }),
+//         headers: { 'Content-Type': 'application/json' }
+//       });
+  
+//       // goes to dashboard if valid login
+//       if (response.ok) {
+//         document.location.replace('/');
+//       } else {
+//         alert(response.statusText);
+//       }
+//     }
+//   }
+
+// document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
   // Once API routes are defined they will be used for login
 
@@ -19,10 +52,10 @@ function LoginForm(props) {
         </h1>
       </div>
       <div class="form">
-        <form class="login-form">
+        <form classname="login-form">
           <input type="text" placeholder="username" id="username-login" />
           <input type="password" placeholder="password" id="password-login" />
-          <button>login</button>
+          <button type="submit">login</button>
           <p class="message">
             Not registered? <a href="RegisterForm">Create an account</a>
           </p>
